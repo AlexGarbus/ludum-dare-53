86f5f392837b414a8c238e9d0e0bd5dbcf2e3316
AlexGarbus <alex.garbus@outlook.com>
2023-04-29 17:12:13 -0400

    Player jump interrupt

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -3,17 +3,20 @@ extends CharacterBody2D
 
 
 @export var _horizontal_speed := 300.0
-@export var _jump_velocity = -500.0
+@export var _jump_velocity := -500.0
+@export var _jump_interrupt_factor := 0.5
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta) -> void:
-	if not is_on_floor():
+	if is_on_floor():
+		if Input.is_action_just_pressed("move_up"):
+			velocity.y = _jump_velocity
+	else:
+		if velocity.y < 0 and Input.is_action_just_released("move_up"):
+			velocity.y *= _jump_interrupt_factor
 		velocity.y += gravity * delta
-
-	if Input.is_action_just_pressed("move_up") and is_on_floor():
-		velocity.y = _jump_velocity
-
+	
 	var direction = round(Input.get_axis("move_left", "move_right"))
 	velocity.x = direction * _horizontal_speed
 
