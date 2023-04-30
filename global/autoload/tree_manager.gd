9e028da328f8e9dcae4e96e4fac0bf2f6dfb9b42
AlexGarbus <alex.garbus@outlook.com>
2023-04-30 18:22:33 -0400

    Disable quit on web

--- global/autoload/tree_manager.gd
+++ global/autoload/tree_manager.gd
@@ -2,5 +2,5 @@ extends Node
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("quit"):
+	if OS.get_name() != "Web" and event.is_action_pressed("quit"):
 		get_tree().quit()
