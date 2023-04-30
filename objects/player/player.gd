47ce4e75c5184f38ed52091664b072c83e205f27
AlexGarbus <alex.garbus@outlook.com>
2023-04-29 20:16:38 -0400

    Player movement refactor

--- objects/player/player.gd
+++ objects/player/player.gd
@@ -8,16 +8,18 @@ extends CharacterBody2D
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
+func _unhandled_input(event: InputEvent) -> void:
+	if is_on_floor() and event.is_action_pressed("move_up"):
+		velocity.y = _jump_velocity
+	elif velocity.y < 0 and event.is_action_pressed("move_up"):
+		velocity.y *= _jump_interrupt_factor
+
+
 func _physics_process(delta) -> void:
-	if is_on_floor():
-		if Input.is_action_just_pressed("move_up"):
-			velocity.y = _jump_velocity
-	else:
-		if velocity.y < 0 and Input.is_action_just_released("move_up"):
-			velocity.y *= _jump_interrupt_factor
+	if !is_on_floor():
 		velocity.y += gravity * delta
 	
-	var direction = round(Input.get_axis("move_left", "move_right"))
+	var direction = InputUtility.get_axis_ceil("move_left", "move_right")
 	velocity.x = direction * _horizontal_speed
 
 	move_and_slide()
