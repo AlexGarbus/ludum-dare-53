8669098284a5482dde332bfd57760621a4a78b77
AlexGarbus <alex.garbus@outlook.com>
2023-05-04 22:20:12 -0400

    Handle Game end using timer

--- running_scenes/game/game.gd
+++ running_scenes/game/game.gd
@@ -2,8 +2,9 @@ class_name Game
 extends Node
 
 
-@onready var _camera: GameCamera = %GameCamera
-@onready var _player: Player = %Player
+var ended := false
+
+@onready var _end_timer: Timer = %EndTimer
 
 
 func _ready() -> void:
@@ -12,6 +13,11 @@ func _ready() -> void:
 
 
 func _on_finish_area_body_entered(body: Node2D) -> void:
-	if body is Player:
+	if !ended and body is Player:
+		ended = true
 		PlayerData.game_stopwatch.started = false
-		get_tree().change_scene_to_file(ScenePaths.END)
+		_end_timer.start()
+
+
+func _on_end_timer_timeout() -> void:
+	get_tree().change_scene_to_file(ScenePaths.END)
