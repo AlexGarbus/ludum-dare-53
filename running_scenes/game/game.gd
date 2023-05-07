4d807fc4f4dbeed9b22918b1af2f3f667e648ce2
AlexGarbus <alex.garbus@outlook.com>
2023-05-07 15:57:49 -0400

    Revert "Handle Game end using timer"
    This reverts commit 8669098284a5482dde332bfd57760621a4a78b77.

--- running_scenes/game/game.gd
+++ running_scenes/game/game.gd
@@ -2,9 +2,8 @@ class_name Game
 extends Node
 
 
-var ended := false
-
-@onready var _end_timer: Timer = %EndTimer
+@onready var _camera: GameCamera = %GameCamera
+@onready var _player: Player = %Player
 
 
 func _ready() -> void:
@@ -13,11 +12,6 @@ func _ready() -> void:
 
 
 func _on_finish_area_body_entered(body: Node2D) -> void:
-	if !ended and body is Player:
-		ended = true
+	if body is Player:
 		PlayerData.game_stopwatch.started = false
-		_end_timer.start()
-
-
-func _on_end_timer_timeout() -> void:
-	get_tree().change_scene_to_file(ScenePaths.END)
+		get_tree().change_scene_to_file(ScenePaths.END)
