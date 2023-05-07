cab153c5790185bc1d650baf9f789e00e8e6a797
AlexGarbus <alex.garbus@outlook.com>
2023-05-06 22:54:57 -0400

    Fullscreen toggle is exclusive

--- global/autoload/screen_manager.gd
+++ global/autoload/screen_manager.gd
@@ -3,7 +3,7 @@ extends Node
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("window_mode"):
-		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 		else:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
